4ca79da62a9020961d33231a68a0d664996a1bd6
Shayan Eskandari <shayanbahal@gmail.com>
2020-05-31 20:59:47 -0400

    Remove contact us + change to spotify & soundcloud link instead of iframe

--- js/freelancer.js
+++ js/freelancer.js
@@ -15,6 +15,7 @@ $(function() {
     });
 });
 
+
 // Floating label headings for the contact form
 $(function() {
     $("body").on("input propertychange", ".floating-label-form-group", function(e) {
